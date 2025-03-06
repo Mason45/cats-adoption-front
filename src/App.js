4ca30fc260d7f67bb73e38e834856a3cdd6462ec
Mason <chungmanchan00@gmail.com>
2025-03-07 00:12:20 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Button, Card } from 'antd';
+import { Typography, Container } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Typography variant="h3" gutterBottom>
+        Welcome to My Website
+      </Typography>
+      <Card title="Ant Design Card" style={{ width: 300, marginBottom: 20 }}>
+        <p>This is a card from Ant Design.</p>
+        <Button type="primary">Ant Design Button</Button>
+      </Card>
+      <Typography variant="body1" style={{ marginTop: 20 }}>
+        This text is styled with MUI.
+      </Typography>
+    </Container>
   );
 }
 
